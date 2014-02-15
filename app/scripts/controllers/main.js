ff969f62c16cc03d15e47d9e9f3c8d64e40adba4
tbk <theborakompanioni@gmail.com>
2014-02-15 14:05:55 +0100

    initial commit: raw yo project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularChessboardApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
